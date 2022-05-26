6f7eb944b443a173ed9d913ef5b7aab7302f5ed5
Tyler Donison <86095868+tylerdonison@users.noreply.github.com>
2022-05-26 17:52:54 -0600

    Create display.py

---
+++ display.py
@@ -0,0 +1,20 @@
+from turtle import width
+import pygame
+import os
+pygame.font.init()
+pygame.mixer.init()
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First Game")
+
+class Display():
+  //class to handle game display.
+  
+  
+  __init__():
+    pass
+  
+  draw_window():
+    
+    
+    pygame.display.update()
