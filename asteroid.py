efb516a2e95e696e8ca588307adb62a0d0cbfa19
Tyler Donison <86095868+tylerdonison@users.noreply.github.com>
2022-05-26 19:06:11 -0600

    Create asteroid.py

---
+++ asteroid.py
@@ -0,0 +1,19 @@
+#Currently overseen by Tyler
+#Class to oversee the behavior of asteroids
+#Typing solutions to be done elsewhere.
+
+from math import Math
+from words import Words
+
+class Asteroid():
+  
+  __init__():
+    pass
+  
+  #display
+  
+  #behavior
+  
+  #change according to imported module
+  
+  #display problems?
