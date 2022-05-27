912af6c257f7f3c1bf0d155dfde104260da9de6c
Tyler Donison <86095868+tylerdonison@users.noreply.github.com>
2022-05-27 14:49:09 -0600

    Update and rename cannon.py to space_station.py

--- space_station.py
+++ space_station.py
@@ -1,6 +1,6 @@
 ##Class to handle the cannon object. The cannon will react to the userclass's controls. This class will handle the visuals and the bullet objects.
 
-class Cannon():
+class Space_Station():
   
   __init__():
     pass
