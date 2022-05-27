4ecb1e11d9a9c525107398644576ffc744af0d2b
Tyler Donison <86095868+tylerdonison@users.noreply.github.com>
2022-05-26 18:18:58 -0600

    Create cannon.py

---
+++ cannon.py
@@ -0,0 +1,10 @@
+##Class to handle the cannon object. The cannon will react to the userclass's controls. This class will handle the visuals and the bullet objects.
+
+class Cannon():
+  
+  __init__():
+    pass
+  
+  #shooting logic
+  #shooting assets
+  #handle bullets
