0fe292ecc3717041264a1ea78f72f68d2125e0d8
Tyler Donison <86095868+tylerdonison@users.noreply.github.com>
2022-05-26 19:21:34 -0600

    Create word.py

---
+++ word.py
@@ -0,0 +1,20 @@
+import random
+#programming overseen by Jake
+#data for difficulties overseen by Cheryl
+
+#class for handling word module, will include difficultly, problems, etc.
+
+class Word():
+  
+  __init__(difficulty):
+    word = ""
+    difficulty = ""
+    word_list = generate_word_list(difficulty)
+    
+    generate_word_list(difficulty):
+      word_list = []
+      
+      return word_list
+    
+    #generate_sentence(nouns, verbs, subjects):
+    
