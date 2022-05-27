71b4816a209cb0c110bcc27d1868e4dd720e0222
Tyler Donison <86095868+tylerdonison@users.noreply.github.com>
2022-05-26 19:18:26 -0600

    Create math.py

---
+++ math.py
@@ -0,0 +1,17 @@
+#programming overseen by Jake
+#data for difficulties overseen by Cheryl
+
+#class for handling math module, will include difficultly, problems, etcs.
+
+class Math():
+  
+  __init__():
+    var 1 = 0
+    var 2 = 0
+    operator = ""
+    solution = 0
+    
+    set_var():
+      pass
+    
+    #logic for subtraction and division
