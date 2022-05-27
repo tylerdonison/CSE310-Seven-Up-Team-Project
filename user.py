5cf47a4f3bac7d09979d6d5edf2287680cfc09c4
Tyler Donison <86095868+tylerdonison@users.noreply.github.com>
2022-05-26 19:23:39 -0600

    Create user.py

---
+++ user.py
@@ -0,0 +1,13 @@
+#class to handle the user inputs in game.
+
+class User():
+  
+  __init__():
+    pass
+  #display typed text
+  #controls
+  #sounds
+  #cannon class
+  
+  #handle if word or solution is right, delete asteroid if true, maybe have indicator for failed word/solution
+  
