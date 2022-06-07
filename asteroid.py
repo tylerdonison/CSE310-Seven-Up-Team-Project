852921d81b7c7ba37685db40c17ece46c513602c
Tyler Donison <86095868+tylerdonison@users.noreply.github.com>
2022-06-06 21:56:25 -0600

    Add files via upload

---
+++ asteroid.py
@@ -0,0 +1,51 @@
+import random
+from mathematics import Mathematics
+from words import Word
+from user import User
+from constants import *
+
+class Asteroid():
+
+    def __init__(self):#, problem):
+        image_1 = pygame.image.load(os.path.join("Assets", "Pictures", "Space-Station", "Space Station.png"))
+        image_2 = pygame.image.load(os.path.join("Assets", "Pictures", "Space-Station", "Space Station.png"))
+        image_3 = pygame.image.load(os.path.join("Assets", "Pictures", "Space-Station", "Space Station.png"))
+        self.images =[image_1, image_2, image_3]
+        self.used_image = "" #This allows a pygame call of WIN.blit(Asteroid.usedimage, (asteroid.x, asteroid.y))
+        self.size = (100, 100)
+        self.rotation = 0
+        self.y = -200
+        self.x = 0
+        #self.problem = problem
+        #self.user = User()
+
+    def randomize(self, randomize_size=False):
+        if randomize_size:
+            random_size = random.randint(100,200)
+            self.size = (random_size, random_size)
+        else:
+            self.size = self.size_by_word()
+        self.rotation = random.randint(0,361)
+        self.used_image = pygame.transform.rotate(pygame.transform.scale(self.images[random.randint(0,2)], self.size), self.rotation)
+        left_limit = BORDER_DISTANCE
+        right_limit = (WIDTH - (BORDER_DISTANCE*10))
+        self.x = random.randint(left_limit, right_limit)
+        
+    """def size_by_word(self):
+        length = len(self.problem)
+        self.size = length * LETTER_SIZE
+    """
+
+    def handle_movement(self):
+        self.y += ASTEROID_VEL
+        if self.rotation < 360:
+            self.rotation +=1
+        else:
+            self.rotation = 0
+        self.used_image = pygame.transform.rotate(pygame.transform.scale(self.images[random.randint(0,2)], self.size), self.rotation)
+    
+    def handle_collision(self):
+        if self.problem.check_solution(self.user.typed_text()):
+            del self
+    
+        
