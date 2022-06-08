e14b6f8a979a5b525182912002f278efef4293d1
Samuel Casellas <sam.casellas@gmail.com>
2022-06-08 17:55:20 -0600

    Added asset paths

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 
-<<<<<<< HEAD
-
-def main():
-  """main file to direct user to the menu, game loop, etc."""
-=======
 import constants
 from asteroid import Asteroid
 from problem import Problem
@@ -11,7 +6,6 @@ from problem import Problem
 def main():
   """Directs user to the menu, game loop, etc.
   """
->>>>>>> 0eec001... my understanding
   #go to menu
   print(constants.ASSET_PATH)
   problem = Problem()
