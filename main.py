bd78ef025c1db155c3f94b769f479d82313c4c72
Samuel Casellas <100746735+SamuelCasellas@users.noreply.github.com>
2022-06-15 20:27:33 -0600

    Connecting menu to starting point

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 
 import constants
+from menu import Menu
 from asteroid import Asteroid
 from problem import Problem
 
 def main():
   """Directs user to the menu, game loop, etc.
   """
+
   #go to menu
-  print(constants.ASSET_PATH)
-  problem = Problem()
-  Asteroid(problem)
+  menu = Menu()
+  #problem = Problem()
+  #Asteroid(problem)
 
 if __name__ == "__main__":
   main()
