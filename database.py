46dc76e248b0221a8a5bc224625c78a6fb64eca4
Tyler Donison <86095868+tylerdonison@users.noreply.github.com>
2022-07-14 20:31:16 -0600

    Add files via upload

--- database.py
+++ database.py
@@ -1,15 +1,41 @@
-"""This class will be responsible for handling the database, in storing player, 
-date played, and score."""
+from datetime import datetime
+import os
 
-import pandas as pd
-from datetime import date
+class database:
 
-class Database():
     def __init__(self):
-        self.stats = {"Initials": [], "Date": [], "Score": []} #feel free to add more.
-        self.data = pd.DataFrame(self.stats)
+        self.data = dict()
+        self.file_name = "data.txt"
     
-    def add_player(initials, score):
-        date = date.today()
-        new_data = [initials, date, score] #this needs to match the stats dictionary as to merge it with the dataframe
-        self.data.loc[len(data.index)] = new_data
+    def load_data(self):
+        """Loads the data from data.txt and stores it in a dictionary for later use.
+        The dictionary is stored datetime : [initials, score] and can be called as such.
+        """
+        #load any data from the data.txt and place it into a dictionary.
+        with open(self.file_name, "r") as f:
+            for line in f:
+                currentline = line.split(",")
+                self.data[currentline[1]] = [currentline[0], currentline[2]]
+            print(self.data)
+        
+    def save_data(self, initials, score):
+        """Save a new line of data to the data.txt, if the txt doesn't exist it will create it.
+        The data is stored as a comma seperated line. The \n is necessary to create a new line of saved data.
+        """
+        now = datetime.now()
+        saved_datetime = now.strftime("%d/%m/%Y %H:%M:%S")
+        #put data into the dictionary
+        saved_list = [initials, score]
+        self.data[saved_datetime] = saved_list
+        #use data and save it to the data.txt
+        with open(self.file_name, "a") as f:
+            f.write(f"{initials}, {saved_datetime}, {score}, \n")
+    
+    def clear_data(self):
+        os.remove(self.file_name)
+        
+db = database()
+from random import randint
+random_score = randint(0,1000)
+db.save_data("TSD", random_score)
+db.load_data()
